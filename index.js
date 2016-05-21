1af8b0d9e1c29bf3de9337a3cf41f23915550ec0
zdz <1696498456@qq.com>
2016-05-21 23:04:47 +0800

    ~

--- index.js
+++ index.js
@@ -2,23 +2,23 @@
 
 
     var api = {
-        install(Vue, options) {
+        install:function(Vue, options) {
 
-            Vue.merge_component = (ids, requires) => {
+            Vue.merge_component = function(ids, requires) {
 
                 var _Promise = Vue.Promise || Promise ,
                     _res = [] , load
                 if (!_Promise) console.error('Browser does not support Promise , If you use "vue-resource" you can put it in the back of the Vue.use("vue-resource") ')
 
-                load = new _Promise((resolve, reject) => {
+                load = new _Promise(function(resolve, reject) {
                     // setTimeout(() => {
                         requires(resolve, reject)
                     // }, 1000)
                 })
                 
-                ids.forEach((t, i) => {
-                    Vue.component(t, (resolve) => {
-                        load.then((res) => {
+                ids.forEach(function(t, i) {
+                    Vue.component(t, function(resolve) {
+                        load.then(function(res) {
                             _res = res || _res
                             resolve(_res[i])
                         })
